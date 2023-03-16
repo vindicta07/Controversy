c0ef9f441cf0a7acf0f87c2573aceb7dbf5ddda9
кяυиαl pαтнαк <123617337+vindicta07@users.noreply.github.com>
2023-03-16 20:49:27 +0530

    temp variable is now used in the code
    @Divyanshu-Modi now your comment is been taken into considerations and commits are been made!

--- Tempreature Conversion Program.c
+++ Tempreature Conversion Program.c
@@ -8,18 +8,30 @@ int main()
     char unit;
     float temp;
 
-    printf("\n Is this the Temperature in (Fahrenheit) or (Celcius)");
+    printf("\n Is this the Temperature in (Fahrenheit) or (Celcius): ");
     scanf("%c", &unit);
     unit = toupper(unit);
 
     if(unit == 'C')
     {
         printf("\n The Temperature is Currently in Celcius! \n ");
+        printf("\n Enter Temperature in Celsius: ");
+        scanf("%f", &temp);
+        
+        temp = (temp * 9 / 5) + 32;
+        printf("\n The Temperature in Fahrenheit is: %.1f ",temp);
+
     }
 
     else if(unit == 'F')
     {
         printf("\n The Temperature is Currently in Fahrenheit! \n ");
+        printf("\n Enter Temperature in Fahrenheit: ");
+        scanf("%f", &temp);
+
+        temp = ((temp - 32)*5)/9;
+        printf("\n The Temperature in Celsius is: %.1f",temp);
+    
     }
 
     else
